7777f196f45453a5dcdd9982542273f50985e343
Subin Mathew <subin@qreative.ai>
2025-05-13 22:45:12 +1000

    the pause was breaking scheduler

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/Rs2InventorySetup.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/util/Rs2InventorySetup.java
@@ -103,7 +103,7 @@ public class Rs2InventorySetup {
             }
 
             if (!Rs2Bank.hasBankItem(lowerCaseName, withdrawQuantity,false)) {
-                Microbot.pauseAllScripts = true;
+                Microbot.log("Bank is missing the following item " + inventorySetupsItem.getName());
                 Microbot.showMessage("Bank is missing the following item " + inventorySetupsItem.getName(), 10);
                 break;
             }
